3924f06830b1bb4db39eea389811bea57c36e3ad
ricardoriballo <99474925+ricardoriballo@users.noreply.github.com>
2022-04-18 11:36:47 +0200

    host

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 })
 
 app.use(cors({
-    origin: ["*"],
+    origin: ["https://co-working-back.vercel.app/api/", "http://localhost:3000", "http://localhost:4200" ],
     credentials: true
 }));
 
